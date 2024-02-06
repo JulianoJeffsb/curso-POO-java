789fb4f81f2f5cf047d7606e5ec60bbdb61074fc
JulianoJeffsb <juliano.jeff.sb@gmail.com>
2024-02-06 00:27:43 -0300

    criando pasta para dividir as aulas

---
+++ aula03/Aula03.java
@@ -0,0 +1,11 @@
+public class Aula03 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.status();
+    
+
+
+    }
+}
+//  javac Aula02.java Caneta.java
+//  java Aula02
