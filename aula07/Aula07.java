ffdac7934187da4b700246b3c17138fb468f5fa2
JulianoJeffsb <juliano.jeff.sb@gmail.com>
2024-02-10 21:29:14 -0300

    Objetos Compostos

---
+++ aula07/Aula07.java
@@ -0,0 +1,15 @@
+public class Aula07 {
+    public static void main(String[] args) {
+        Lutador l[] = new Lutador[6]; 
+        l[0] = new Lutador("Pretty boy", "frança", 31, 1.75f, 68.9f, 11, 2, 1);
+        l[1] = new Lutador("Putscript", "Brasil", 29, 1.68f, 57.8f, 11, 2, 1);
+        l[2] = new Lutador("Snapshadow", "EUA", 31, 1.75f, 80.9f, 11, 2, 1);
+        l[3] = new Lutador("Dead Code", "Austrália", 31, 1.75f, 81.9f, 11, 2, 1);
+        l[4] = new Lutador("UFOCobol", "Brasil", 31, 1.75f, 119.9f, 11, 2, 1);
+        l[5] = new Lutador("Nerdaart", "EUA", 31, 1.75f, 105.9f, 11, 2, 1);
+        l[3].status();
+    }
+}
+// javac Aula07.java Controlador.java Lutador.java
+//java Aula07
+//javac Aula07.java Controlador.java Lutador.java && java Aula07
