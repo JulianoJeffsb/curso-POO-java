ebf7366c30dba4b27dd42b6eeb80e8572cc26885
JulianoJeffsb <juliano.jeff.sb@gmail.com>
2024-02-14 09:19:11 -0300

    Resolvido

--- aula10/ProjetoPessoas.java
+++ aula10/ProjetoPessoas.java
@@ -8,13 +8,19 @@ public class ProjetoPessoas {
         Funcionario p4 = new Funcionario();
 
         p1.setNome("Pedro");
-        p2.setNome("Maria");
-        p3.setNome("Cláudio");
-        p4.setNome("Fabiana");
-
+        p1.setIdade(20);
         p1.setSexo("M");
+        p2.setNome("Maria");
+        p2.setIdade(23);
+        p2.setSexo("F");
+        p3.setNome("Cláudio");
+        p3.setIdade(21);
+        p3.setSexo("M");
+        p4.setNome("Fabiana");
+        p4.setIdade(19);
         p4.setSexo("F");
-        p2.setIdade(18);
+
+        
 
         System.out.println(p1.toString());
         System.out.println(p2.toString());
