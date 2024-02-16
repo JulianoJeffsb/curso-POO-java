db0ba2a58553861a24caf1a45e12d890f81f7bc6
JulianoJeffsb <juliano.jeff.sb@gmail.com>
2024-02-16 16:41:20 -0300

    problema

--- aula12/Animal.java
+++ aula12/Animal.java
@@ -1,4 +1,4 @@
-package aula12;
+
 
 public abstract class Animal
 {
@@ -10,5 +10,25 @@ public abstract class Animal
     public abstract void alimentar();
     public abstract void emitirSom();
 
+    public float getPeso() {
+        return peso;
+    }
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public int getMembros() {
+        return membros;
+    }
+    public void setMembros(int membros) {
+        this.membros = membros;
+    }
+
+    
 
 }
--- aula12/Aula12.java
+++ aula12/Aula12.java
@@ -1,9 +1,19 @@
-package aula12;
 
 public class Aula12 
 {
     public static void main(String[]args)
     {
+        //Animal n = new Animal();
+        Mamifero m = new Mamifero();
+        Reptil r = new Reptil();
+        Peixe p = new Peixe();
+        Ave a = new ave();
 
+        m.setPeso(25.5f);
+        m.setCorPelo("Marrom");
+        m.alimentar();
+        m.locomover();
+        m.emitirSom();
+    
     }
 }
