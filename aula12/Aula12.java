ff02535d770787a9484c04d08bbe688fdffbb471
JulianoJeffsb <juliano.jeff.sb@gmail.com>
2024-02-22 09:45:46 -0300

    fim

--- aula12/Aula12.java
+++ aula12/Aula12.java
@@ -7,13 +7,37 @@ public class Aula12
         Mamifero m = new Mamifero();
         Reptil r = new Reptil();
         Peixe p = new Peixe();
-        Ave a = new ave();
+        Ave a = new Ave();
 
+        System.out.println("Características dos mamíferos: ");
         m.setPeso(25.5f);
         m.setCorPelo("Marrom");
         m.alimentar();
         m.locomover();
         m.emitirSom();
-    
+
+        System.out.println("\nCaracterísticas dos répteis: ");
+        r.setPeso(5.5f);
+        r.setCorEscama("Verde");
+        r.alimentar();
+        r.locomover();
+        r.emitirSom();
+
+        System.out.println("\nCaracterísticas dos peixes: ");
+        p.setPeso(15.5f);
+        p.setCorEscama("Cinsa");
+        p.alimentar();
+        p.locomover();
+        p.emitirSom();
+
+        System.out.println("\nCaracterísticas das aves: ");
+        a.setPeso(25.5f);
+        a.setCorPena("Branca");
+        a.alimentar();
+        a.locomover();
+        a.emitirSom();
+
+        
+        
     }
 }
